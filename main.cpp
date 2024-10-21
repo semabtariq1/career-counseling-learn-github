477c9f37669790c4ee2d1d1eec86259c917942e9
mahatariq11 <mahaatariq.1@gmail.com>
2024-10-21 20:55:48 +0500

    update

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"Enter any number: ";
+cin>>num;
+if(num==0)
+cout<<"The number you have entered is equal to zero";
+else
+cout<<"Invalid number";
+return 0;
+}
