d8534266231508bddaebdccb392af0f12e1c7cfd
usamasabir <usamasabir680@gmail.com>
2024-10-22 07:09:39 -0700

    update a function of even

--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@ cout<<"Enter any number: ";
 cin>>num;
 if(num==0)
 cout<<"The number you have entered is equal to zero";
-else
-cout<<"Invalid number";
+else if
+(num % 2==0) {
+cout<< " The number you have entered is even: ";
+}
+else {
+cout<<"invalid number: ";
+}
+
 return 0;
 }
